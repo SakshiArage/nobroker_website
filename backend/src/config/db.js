b8c1465ecaa69c3df27ba152070a0c4e3944d91d
Raja-55 <contactraja44@gmail.com>
2026-08-02 22:14:36 +0530

    Update database configuration

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  const mongoUri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/nobroker";
+  const mongoUri = process.env.MONGO_URI ;
   globalThis.__dbAvailable = false;
 
   try {
